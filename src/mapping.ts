cbcfb6687fd54fb8fbc7b6a83a4b0df7b27c8d02
paulvaden <pbdonut@gmail.com>
2022-12-05 11:24:34 -0500

    Staking changes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,34 +1,17 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 import {
-  Cooldown as CooldownEvent,
-  CooldownUpdated,
-  DelegateChanged,
-  DelegatedPowerChanged,
-  Redeem as RedeemEvent,
-  RewardsAccrued,
-  RewardsClaimed,
+  Withdrawn as WithdrawnEvent,
   Staked,
-  Transfer,
-} from "../generated/templates/LyraSafetyModule/LyraSafetyModule";
+  
+} from "../generated/stkLyraProxy/StakingRewards";
 import {
-  Initialize1Call,
-  Upgraded,
-} from "../generated/stkLyraProxy/LyraSafetyModuleProxy";
-import {
-  TokenTransfer,
   User,
   Stake,
-  Redeem,
-  Cooldown,
+  Withdrawn,
 } from "../generated/schema";
-import { LyraSafetyModule as LyraSafetyModuleTemplate } from "../generated/templates";
 
 let ZERO = BigInt.fromI32(0);
-
-export function handleUpgrade(event: Upgraded): void {
-  LyraSafetyModuleTemplate.create(event.params.implementation);
-}
 
 function loadOrCreateUser(userAddress: string): User {
   let user = User.load(userAddress);
@@ -40,55 +23,10 @@ function loadOrCreateUser(userAddress: string): User {
   return user;
 }
 
-export function handleTransfer(event: Transfer): void {
-  let from = event.params.from;
-  let to = event.params.to;
-  let value = event.params.value;
-
-  let userFrom = loadOrCreateUser(from.toHex());
-  let userTo = loadOrCreateUser(to.toHex());
-
-  let transfer = new TokenTransfer(
-    event.transaction.hash.toHex() + event.logIndex.toString()
-  );
-
-  transfer.txHash = event.transaction.hash;
-  transfer.blockNumber = event.block.number.toI32();
-  transfer.timestamp = event.block.timestamp.toI32();
-  transfer.to = userTo.id;
-  transfer.from = userFrom.id;
-  transfer.amount = value;
-  transfer.save();
-
-  userFrom.balance = userFrom.balance.minus(event.params.value);
-  userTo.balance = userTo.balance.plus(event.params.value);
-
-  userFrom.save();
-  userTo.save();
-}
-
-export function handleCooldownUpdated(event: CooldownUpdated): void {
+export function handleWithdrawn(event: WithdrawnEvent): void {
   let userAddress = event.params.user.toHex();
   loadOrCreateUser(userAddress);
-  let cooldown = new Cooldown(
-    event.transaction.hash.toHex() + event.logIndex.toString()
-  );
-  cooldown.txHash = event.transaction.hash;
-  cooldown.blockNumber = event.block.number.toI32();
-  cooldown.timestamp = event.block.timestamp.toI32();
-  cooldown.user = userAddress;
-  cooldown.amount = event.params.balance;
-  cooldown.cooldownTimestamp = event.params.cooldownTimestamp.toI32();
-  cooldown.save();
-}
-
-export function handleDelegatedPowerChanged(
-  event: DelegatedPowerChanged
-): void {}
-export function handleRedeem(event: RedeemEvent): void {
-  let userAddress = event.params.from.toHex();
-  loadOrCreateUser(userAddress);
-  let redeem = new Redeem(
+  let redeem = new Withdrawn(
     event.transaction.hash.toHex() + event.logIndex.toString()
   );
   redeem.txHash = event.transaction.hash;
@@ -100,7 +38,7 @@ export function handleRedeem(event: RedeemEvent): void {
 }
 
 export function handleStaked(event: Staked): void {
-  let userAddress = event.params.onBehalfOf.toHex();
+  let userAddress = event.params.user.toHex();
   loadOrCreateUser(userAddress);
   let stake = new Stake(
     event.transaction.hash.toHex() + event.logIndex.toString()
@@ -112,9 +50,3 @@ export function handleStaked(event: Staked): void {
   stake.amount = event.params.amount;
   stake.save();
 }
-
-export function handleRewardsAccrued(event: RewardsAccrued): void {}
-export function handleRewardsClaimed(event: RewardsClaimed): void {}
-
-export function handleCooldown(event: CooldownEvent): void {}
-export function handleDelegateChanged(event: DelegateChanged): void {}
--- subgraph.js
+++ subgraph.js
@@ -1,10 +1,8 @@
 const path = require("path");
 
-const stkLyraDeployBlock = 15801447;
-const stkLyraProxy = "0xdE48b1B5853cc63B1D05e507414D3E02831722F8";
-const initialStakeLyra = "0xB0B06F0440ce13d03D41c5399C55AEfEa1b372CE";
+const deployBlock = 5048780;
+const stakingContract = "0xb02e538a08cFA00E9900cf94e33B161323d8D162";
 
-const registryAddress = "0xF5A0442D4753cA1Ea36427ec071aa5E786dA5916";
 const network = "optimism";
 
 const dataSources = [
@@ -13,9 +11,9 @@ const dataSources = [
     name: "stkLyraProxy",
     network,
     source: {
-      address: stkLyraProxy,
-      startBlock: stkLyraDeployBlock,
-      abi: "LyraSafetyModuleProxy",
+      address: stakingContract,
+      startBlock: deployBlock,
+      abi: "StakingRewards",
     },
     mapping: {
       kind: "ethereum/events",
@@ -25,137 +23,18 @@ const dataSources = [
       entities: ["TokenTransfer"], //This value is currently not used by TheGraph at all, it just cant be empty
       abis: [
         {
-          name: "LyraSafetyModuleProxy",
-          file: "./abis/LyraSafetyModuleProxy.json",
+          name: "StakingRewards",
+          file: "./abis/StakingRewards.json",
         },
       ],
       eventHandlers: [
         {
-          event: "Upgraded(indexed address)",
-          handler: "handleUpgrade",
-        },
-      ],
-    },
-  },
-  {
-    kind: "ethereum/contract",
-    name: "LyraSafetyModule",
-    network,
-    source: {
-      address: stkLyraProxy,
-      startBlock: stkLyraDeployBlock,
-      abi: "LyraSafetyModule",
-    },
-    mapping: {
-      kind: "ethereum/events",
-      apiVersion: "0.0.5",
-      language: "wasm/assemblyscript",
-      file: "./src/mapping.ts",
-      entities: ["TokenTransfer"], //This value is currently not used by TheGraph at all, it just cant be empty
-      abis: [
-        {
-          name: "LyraSafetyModule",
-          file: "./abis/LyraSafetyModule.json",
-        },
-      ],
-      eventHandlers: [
-        {
-          event: "Transfer(indexed address,indexed address,uint256)",
-          handler: "handleTransfer",
-        },
-        {
-          event: "CooldownUpdated(indexed address,uint256,uint256)",
-          handler: "handleCooldownUpdated",
-        },
-        {
-          event: "Cooldown(indexed address)",
-          handler: "handleCooldown",
-        },
-        {
-          event: "DelegateChanged(indexed address,indexed address,uint8)",
-          handler: "handleDelegateChanged",
-        },
-        {
-          event: "DelegatedPowerChanged(indexed address,uint256,uint8)",
-          handler: "handleDelegatedPowerChanged",
-        },
-        {
-          event: "Redeem(indexed address,indexed address,uint256)",
-          handler: "handleRedeem",
-        },
-        {
-          event: "RewardsAccrued(address,uint256)",
-          handler: "handleRewardsAccrued",
-        },
-        {
-          event: "RewardsClaimed(indexed address,indexed address,uint256)",
-          handler: "handleRewardsClaimed",
-        },
-        {
-          event: "Staked(indexed address,indexed address,uint256)",
+          event: "Staked(indexed address,uint256)",
           handler: "handleStaked",
         },
-      ],
-    },
-  },
-];
-
-const templates = [
-  {
-    kind: "ethereum/contract",
-    name: "LyraSafetyModule",
-    network,
-    source: {
-      abi: "LyraSafetyModule",
-    },
-    mapping: {
-      kind: "ethereum/events",
-      apiVersion: "0.0.5",
-      language: "wasm/assemblyscript",
-      file: "./src/mapping.ts",
-      entities: ["TokenTransfer"], //This value is currently not used by TheGraph at all, it just cant be empty
-      abis: [
         {
-          name: "LyraSafetyModule",
-          file: "./abis/LyraSafetyModule.json",
-        },
-      ],
-      eventHandlers: [
-        {
-          event: "Transfer(indexed address,indexed address,uint256)",
-          handler: "handleTransfer",
-        },
-        {
-          event: "CooldownUpdated(indexed address,uint256,uint256)",
-          handler: "handleCooldownUpdated",
-        },
-        {
-          event: "Cooldown(indexed address)",
-          handler: "handleCooldown",
-        },
-        {
-          event: "DelegateChanged(indexed address,indexed address,uint8)",
-          handler: "handleDelegateChanged",
-        },
-        {
-          event: "DelegatedPowerChanged(indexed address,uint256,uint8)",
-          handler: "handleDelegatedPowerChanged",
-        },
-        {
-          event: "Redeem(indexed address,indexed address,uint256)",
-          handler: "handleRedeem",
-        },
-        {
-          event: "RewardsAccrued(address,uint256)",
-          handler: "handleRewardsAccrued",
-        },
-        {
-          event: "RewardsClaimed(indexed address,indexed address,uint256)",
-          handler: "handleRewardsClaimed",
-        },
-        {
-          event: "Staked(indexed address,indexed address,uint256)",
-          handler: "handleStaked",
+          event: "Withdrawn(indexed address,uint256)",
+          handler: "handleWithdrawn",
         },
       ],
     },
@@ -170,5 +49,4 @@ module.exports = {
     file: "./schema.graphql",
   },
   dataSources,
-  templates,
 };
